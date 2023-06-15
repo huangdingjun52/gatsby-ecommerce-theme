a347fa04782c6dc71e17230e997c0d0297d14dde
huangdingjun52 <134582173+huangdingjun52@users.noreply.github.com>
2023-06-15 15:55:02 +0800

    gatsby-ecommerce-theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'黃鼎鈞個人網頁'}
+        subtitle={'Discover 2023'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
